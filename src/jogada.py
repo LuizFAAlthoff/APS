9af6b0215ec334e51ad31246c976ff493a080264
Luiz Fernando <luizfernando.althoff@gmail.com>
2024-06-29 10:55:36 -0300

    progresso na lógica do jogo

--- src/jogada.py
+++ src/jogada.py
@@ -1,11 +1,17 @@
+from carta_especial import CartaEspecial
 
 
 class Jogada():
-    def __init__(self, jogador):
+    def __init__(self, jogador, carta_recente_tabuleiro):
+        self.__carta_recente_tabuleiro = carta_recente_tabuleiro
         self.__jogador = jogador
         self.__cartas_encadeamento = []
         self.__jogada_vencedora = False
     
+    @property
+    def carta_recente_tabuleiro(self):
+        return self.__carta_recente_tabuleiro
+
     @property
     def jogador(self):
         return self.__jogador
@@ -18,6 +24,10 @@ class Jogada():
     def jogada_vencedora(self):
         return self.__jogada_vencedora
     
+    @carta_recente_tabuleiro.setter
+    def carta_recente_tabuleiro(self, carta_recente_tabuleiro):
+        self.__carta_recente_tabuleiro = carta_recente_tabuleiro
+
     @jogador.setter
     def jogador(self, jogador):
         self.__jogador = jogador
@@ -29,3 +39,105 @@ class Jogada():
     @jogada_vencedora.setter
     def jogada_vencedora(self, jogada_vencedora):
         self.__jogada_vencedora = jogada_vencedora
+
+    def __del__(self):
+        print(f"Jogada do jogador {self.__jogador.nome} foi deletada")
+
+    def ainda_pode_jogar_qtd_maxima(self):
+        if len(self.__cartas_encadeamento) <= 3:
+            return True
+        else:
+            return False
+        
+    def ainda_nao_jogou(self):
+        if len(self.__cartas_encadeamento) == 0:
+            return True
+        else:
+            return False
+    
+    def get_carta_mais_recente(self):
+        return self.__cartas_encadeamento[-1]
+    
+    def verificar_condicao_de_vitoria(self):
+        if len(self.__jogador.mao_cartas) - len(self.__cartas_encadeamento) == 0:
+            self.__jogada_vencedora = True
+            return True
+    
+    def verificar_existencia_especial_no_encadeamento(self):
+        for carta in self.__cartas_encadeamento:
+            if carta.tipo == "especial":
+                return True
+        return False
+
+    def add_carta_encadeamento(self, carta):
+        self.__jogador.mao_cartas.pop(0) #por enquanto, só remove a carta inicial da lista, e não a carta específica da jogada
+        self.__cartas_encadeamento.append(carta)
+
+    def verificar_se_eh_ultima_carta(self):
+        if len(self.__jogador.mao_cartas) == 1:
+            return True
+        else:
+            return False
+
+    def verificar_se_lista_encadeamento_esta_vazia(self):
+        if len(self.__cartas_encadeamento) == 0:
+            return True
+        else:
+            return
+
+    def checar_atributos(self, carta_para_jogar, carta_mais_recente):
+        carta_eh_valida = False
+        if isinstance(carta_mais_recente, CartaEspecial):
+            print("Carta compatível porque a carta anterior é especial")
+            carta_eh_valida = True
+        elif len(self.__cartas_encadeamento) == 0 and carta_para_jogar.numero == carta_mais_recente.numero:
+            print("Carta compatível com a anterior porque é a primeira carta e números são iguais")
+            carta_eh_valida = True
+        elif carta_mais_recente.cor_primaria == carta_para_jogar.cor_primaria:
+            print("Carta compatível com a anterior porque cores primárias são iguais")
+            carta_eh_valida = True
+        elif carta_mais_recente.cor_primaria == carta_para_jogar.cor_secundaria:
+            print("Carta compatível com a anterior porque cores primária e secundária são iguais")
+            carta_eh_valida = True
+        elif carta_mais_recente.cor_secundaria == carta_para_jogar.cor_primaria:
+            print("Carta compatível com a anterior porque cores secundária e primária são iguais")
+            carta_eh_valida = True
+        elif carta_mais_recente.cor_secundaria == carta_para_jogar.cor_secundaria:
+            print("Carta compatível com a anterior porque cores secundárias são iguais")
+            carta_eh_valida = True
+        else:
+            print("Carta incompatível com a anterior")
+            carta_eh_valida = False
+
+        return carta_eh_valida
+            
+        
+
+    def verificar_carta(self, carta_para_jogar):
+        if self.ainda_pode_jogar_qtd_maxima:                        #verifica se seq max ainda não foi atingida
+            if self.verificar_existencia_especial_no_encadeamento():    #caso exista carta especial no encadeamento, retorna False
+                print("Carta especial não pode ser jogada no meio de uma sequência")
+                return False
+            if isinstance(carta_para_jogar, CartaEspecial):             #verifica se a carta a ser jogada é especial
+                if self.verificar_se_eh_ultima_carta():                     #caso a carta especial a ser jogada seja a última, retorna False
+                    print("Carta especial não pode ser jogada como última carta")
+                    return False
+                else:                                                       #caso não seja a última, verifica se a lista de encadeamento está vazia
+                    if self.verificar_se_lista_encadeamento_esta_vazia():       
+                        return True                                             #caso esteja vazia, retorna True
+                    else:                                                       #caso não esteja vazia, retorna False
+                        print("Carta especial não pode ser jogada no meio de uma sequência")
+                        return False
+            else:                                                       #caso a carta a ser jogada não seja especial, ela é normal
+                if self.verificar_se_lista_encadeamento_esta_vazia():       #verifica se a lista de encadeamento está vazia
+                    print("Pegou carta mais recente do tabuleiro")
+                    carta_mais_recente = self.carta_recente_tabuleiro           #pega a carta mais recente a partir do parametro recebido pelo tabuleiro
+                    self.checar_atributos(carta_para_jogar, carta_mais_recente) #checa se a carta a ser jogada é compatível com a carta mais recente
+                else:
+                    print("Pegou carta mais recente da lista de encadeamento")
+                    carta_mais_recente = self.get_carta_mais_recente()          #pega a carta mais recente a partir da lista de encadeamento
+                    if self.checar_atributos(carta_para_jogar, carta_mais_recente): #checa se a carta a ser jogada é compatível com a carta mais recente
+                        return True                                             #caso seja compatível, retorna True
+        else:                                                       #caso a seq max tenha sido atingida, retorna False
+            print("Sequência máxima de cartas atingida")
+            return False
--- src/testessss.py
+++ src/testessss.py
@@ -7,23 +7,50 @@ from window import Window
 from dog.dog_actor import DogActor
 from dog.dog_interface import DogPlayerInterface
 from PIL import Image, ImageTk
-from tabuleiro import Tabuleiro
+import random
+from carta_normal import CartaNormal
+from carta_especial import CartaEspecial
+from jogada import Jogada
 
-class App:
+class AppTeste:
     def __init__(self, root):
-        self.tabuleiro = Tabuleiro()
+        self.tabuleiro = TabuleiroTeste()
         self.root = root
-        self.root.title("Carta Aleatória")
+        self.root.title("Interface de testes")
         self.root.geometry("1280x720")
 
         self.label = Label(root)
         self.label.pack(expand=True)
 
-        self.button = Button(root, text="Mostrar Carta Aleatória", command=self.show_random_card)
-        self.button.pack()
+        self.button_carta_aleatoria = Button(root, text="Passar turno", command=self.passar_turno)
+        self.button_carta_aleatoria.pack()
 
-    def show_random_card(self):
-        carta = self.tabuleiro.get_random_card()
+        self.button1 = Button(self.root, text="Ação Jogador 1", command=lambda: self.realizar_jogada(0))
+        self.button1.pack(pady=5)
+        
+        self.button2 = Button(self.root, text="Ação Jogador 2", command=lambda: self.realizar_jogada(1))
+        self.button2.pack(pady=5)
+        
+        self.button3 = Button(self.root, text="Ação Jogador 3", command=lambda: self.realizar_jogada(2))
+        self.button3.pack(pady=5)
+
+    def realizar_jogada(self, player_id):
+        self.tabuleiro.jogada_atual = Jogada(self.tabuleiro.jogadores[player_id], self.tabuleiro.ultima_carta)
+        if player_id == self.tabuleiro.get_jogador_da_vez():
+            print(f"Jogador {player_id} realizou uma ação!")
+            random_card = self.tabuleiro.get_random_card(self.tabuleiro.lista_cartas)
+            if self.tabuleiro.jogada_atual.verificar_carta(random_card) == True:
+                self.tabuleiro.ultima_carta = random_card
+                self.show_card(self.tabuleiro.ultima_carta)
+            else:
+                print("Carta inválida!")
+        else:
+            print(f"Não é o turno do Jogador {player_id}.")
+
+    def passar_turno(self):
+        self.tabuleiro.mudar_turno(self.tabuleiro.jogada_atual)
+
+    def show_card(self, carta):
         image_path = carta.get_card_image()
 
         image = Image.open(image_path)
@@ -33,7 +60,156 @@ class App:
         self.label.config(image=photo)
         self.label.image = photo
 
-if __name__ == "__main__":
-    root = Tk()
-    app = App(root)
-    root.mainloop()
+class JogadorTeste:
+    def __init__(self, nome: str):
+        self.__nome = nome
+        self.__id = ""
+        self.__mao_cartas = []
+    
+    @property
+    def nome(self):
+        return self.__nome
+    
+    @property
+    def id(self):
+        return self.__id
+
+    @property
+    def mao_cartas(self):
+        return self.__mao_cartas
+    
+    @id.setter
+    def id(self, id):
+        self.__id = id
+
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome
+        
+    @mao_cartas.setter
+    def mao_cartas(self, mao_cartas):
+        self.__mao_cartas = mao_cartas
+    
+    def add_cartas_na_mao(self, lista_carta):
+        self.mao_cartas.extend(lista_carta)
+    
+    def get_cartas_mao(self, index: int):
+        return self.mao_cartas[index]
+
+class TabuleiroTeste:
+    def __init__(self):
+        self.__ultima_carta = None
+        self.__lista_cartas = []
+        self.criar_baralho()
+        self.__contador_cartas_mais_um = 0
+        self.__jogadores = [JogadorTeste("Maria1"), JogadorTeste("Pedro2"), JogadorTeste("Fabi3")]
+        self.__primeira_acao = True
+        self.__jogador_atual = None
+        self.__local_id = ""
+        self.__turno = 0
+        self.__jogada_atual = None
+
+        id = 0
+        for player in self.__jogadores:
+            player.id = id
+            id += 1
+
+        for player in self.__jogadores: #distribui 7 cartas para cada jogador
+            player.add_cartas_na_mao(self.dar_cartas_iniciais())
+    
+    @property
+    def ultima_carta(self):
+        return self.__ultima_carta
+    
+    @property
+    def lista_cartas(self):
+        return self.__lista_cartas
+    
+    @property
+    def contador_cartas_mais_um(self):
+        return self.__contador_cartas_mais_um
+
+    @property
+    def jogadores(self):
+        return self.__jogadores
+
+    @property
+    def primeira_acao(self):
+        return self.__primeira_acao
+    
+    @property
+    def jogador_atual(self):
+        return self.__jogador_atual
+    @property
+    def local_id(self):
+        return self.__local_id
+    
+    @ultima_carta.setter
+    def ultima_carta(self, ultima_carta):
+        self.__ultima_carta = ultima_carta
+        
+    @lista_cartas.setter
+    def lista_cartas(self, lista_cartas):
+        self.__lista_cartas = lista_cartas
+    
+    @contador_cartas_mais_um.setter
+    def contador_cartas_mais_um(self, contador_cartas_mais_um):
+        self.__contador_cartas_mais_um = contador_cartas_mais_um
+
+    @jogadores.setter
+    def jogadores(self, jogadores):
+        self.__jogadores = jogadores
+
+    @primeira_acao.setter
+    def primeira_acao(self, primeira_acao):
+        self.__primeira_acao = primeira_acao
+    
+    @jogador_atual.setter
+    def jogador_atual(self, jogador_atual):
+        self.__jogador_atual = jogador_atual
+
+    @local_id.setter
+    def local_id(self, local_id):
+        self.__local_id = local_id
+
+    def criar_baralho(self):
+        cores_primaria = ["vermelho", "laranja", "amarelo", "verde", "azul", "anil", "roxo"]
+        cores_secundaria = ["roxo",  "anil", "azul", "verde", "amarelo", "laranja", "vermelho"]
+        lista_cartas_comuns = []
+        lista_cartas_especiais = []
+        for cor_primaria in cores_primaria:
+            for cor_secundaria in cores_secundaria:
+                for numero in range(1, 4):
+                    if cor_primaria != cor_secundaria:
+                        lista_cartas_comuns.append(CartaNormal(cor_primaria, cor_secundaria, numero))
+        for numero in range(7):
+            lista_cartas_especiais.append(CartaEspecial('preto', 'mais-um'))
+            lista_cartas_especiais.append(CartaEspecial('preto', 'block'))
+        
+        self.__lista_cartas.extend(lista_cartas_comuns)
+        self.__ultima_carta = self.get_random_card(lista_cartas_comuns)
+        self.__lista_cartas.extend(lista_cartas_especiais)
+
+    def dar_cartas_iniciais(self) -> list:
+        mao_inicial = []
+        for _ in range(7):
+            carta_aleatoria = self.get_random_card(self.__lista_cartas)
+            mao_inicial.append(carta_aleatoria)
+        return mao_inicial
+
+    def get_random_card(self, lista):
+        return random.choice(lista)
+    
+    def mudar_turno(self, jogada_antiga):
+        del jogada_antiga
+        self.__turno = (self.__turno + 1) % len(self.__jogadores) #isso aqui garante que quando chegar no final da lista, ele volta pro começo
+        print("Turno mudou para o jogador ", self.__jogadores[self.__turno].nome)
+        return self.__jogadores[self.__turno]
+
+    def get_jogador_da_vez(self):
+        print("É a vez do jogador ", self.__jogadores[self.__turno].nome)
+        return self.__jogadores[self.__turno].id
+    
+root = Tk()
+app = AppTeste(root)
+root.mainloop()
